c4b36a85f59d9d33e7aee31b3f585c490e20b309
SeungHwan Lee <60887507+saa4563123@users.noreply.github.com>
2022-09-15 11:32:03 +0900

    Create Korean.h

---
+++ Korean.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+
+// 여기부턴 함수 선언 입니다
+#define 본문함수 int main(void)
+#define 표준소속선언 using namespace std
+// 여기부턴 변수형 선언 입니다
+#define 정수 int
+#define 실수 double
+#define 문자열 string
+// 여기부턴 초기화 선언 입니다
+#define 정수배열초기화 {0}
+// 여기부턴 입출력 선언 입니다
+#define 입력 cin
+#define 출력 cout
+#define 과 >>
+#define 와 <<
+// 여기부턴 조건문 선언 입니다
+#define 만약 if
+#define 그런데만약 else if
+#define 아니면 else
+// 여기부턴 반복문 선언 입니다
+#define 단순조건반복 while
+#define 전후조건반복 for
+#define 무한반복 while(true)
+// 여기부턴 연산자 선언 입니다
+#define 더하기 +
+#define 빼기 -
+#define 곱하기 *
+#define 나누기 /
+#define 나머지 %
+#define 는 =
+#define 그리고 &&
+#define 또는 ||
+#define 크다 >
+#define 크거나같다 >=
+#define 작다 <
+#define 작거나같다 >=
+#define 같다 ==
+#define 증가 ++
+#define 감소 --
+// 여기부턴 반환값 선언 입니다
+#define 반환없음 return 0
+#define 반환 return
+// 여기부턴 숫자 선언 입니다
+#define 음수 -
+#define 영 0
+#define 일 1
+#define 이 2
+#define 삼 3
+#define 사 4
+#define 오 5
+#define 육 6
+#define 칠 7
+#define 팔 8
+#define 구 9
+// 여기부턴 문자 선언 입니다
+#define 끝 ;
+#define 범위시작 [
+#define 범위끝 ]
+#define 함수시작 {
+#define 함수끝 }
+#define 조건시작 (
+#define 조건끝 )
+#define 공백 " "
+#define 개행 "\n"
+// ========================= K o L a n g
